5f50ee4e33e40ce92ac146b09d263c63042f678a
“betinablum” <betina.db@gmail.com>
2023-10-10 16:57:31 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Post } from './Post';
+import { Header } from './components/header';
+
+import styles from './App.module.css';
+import './global.css';
+import { Sidebar } from './components/Sidebar';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Betina" 
+            content="Lorem blablabla Lorem blablabla Lorem blablabla"
+          />
+          <Post 
+            author="Rodrigo" 
+            content="Um novo post"
+          />
+        </main>
+      </div>
+    </div>
+    
+  )
+
+}
